a3a55d166333170d3749841b658028d42ca446d8
Claudio Rodríguez <claudio.rp2403@proton.me>
2026-09-13 13:27:24 -0300

    Añadida función para mostrar el tablero

--- src/server.c
+++ src/server.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
 
 struct Game
 {
     char tablero[3][3];
     int iniciado;
     /* 1 Corresponde a partida iniciada*/
-
 };
 
 struct Game *games;
@@ -18,12 +18,27 @@ void vaciar_tablero(struct Game *game)
     {
         for (int y = 0; y < 3; y++)
         {
-            game->tablero[x][y] = 0;
+            game->tablero[x][y] = ' ';
         }
     }
 }
 
-void shutdown_server(){
+void mostrar_tablero(struct Game *game)
+{
+    for (int x = 0; x < 3; x++)
+    {
+        printf(" %c | %c | %c \n",
+               game->tablero[x][0],
+               game->tablero[x][1],
+               game->tablero[x][2]);
+
+        if (x < 2)
+            printf("---+---+---\n");
+    }
+}
+
+void shutdown_server()
+{
     free(games);
 }
 void main(int argc, char const *argv[])
@@ -31,9 +46,5 @@ void main(int argc, char const *argv[])
     games = malloc(5 * sizeof(*games));
     /*Asignamos la memoria para los 5 juegos*/
 
-
-
-
     shutdown_server();
 }
-
