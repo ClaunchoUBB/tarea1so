8271054e5efabae5c6b161f7c0476031afd4a058
Claudio Rodríguez <claudio.rp2403@proton.me>
2026-09-13 21:36:14 -0300

    Gestión de clientes

--- src/server.c
+++ src/server.c
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <strings.h>
+#include <nSystem.h>
 
 /*
 struct sockaddr_in
@@ -14,6 +15,13 @@ struct sockaddr_in
     struct in_addr sin_addr;   Dirección IP
     unsigned char sin_zero[8]; Relleno con 0
 };
+*/
+
+int clientes_conectados = 0;
+int partidas_activas = 0;
+/*
+Se usan variables globales para
+que los subprocesos también puedan leerlas
 */
 
 struct Game
@@ -26,9 +34,20 @@ struct Game
     */
     char tablero[3][3];
     /* 1 Corresponde a partida iniciada*/
+
+    int jugadores[2]
 };
 
 struct Game *games; // Creamos la estructura global para los juegos
+
+struct Cola
+{
+    int clientes_encolados[2];
+    int front;
+    int back;
+};
+
+
 
 void vaciar_tablero(struct Game *game)
 {
@@ -62,7 +81,11 @@ void shutdown_server()
     free(games);
 }
 
-void main(int argc, char const *argv[])
+int ejecutar_partida(struct Game *partida)
+{
+}
+
+void init_server(int argc, char const *argv[])
 {
     struct sockaddr_in direccion_propia;
     direccion_propia.sin_family = AF_INET;
@@ -72,14 +95,14 @@ void main(int argc, char const *argv[])
 
     socklen_t size_direccion_propia = sizeof(direccion_propia);
 
-    int socketfd;
-    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    int socket_server;
+    socket_server = socket(AF_INET, SOCK_STREAM, 0);
     /* Creamos el fichero descriptor del socket*/
 
-    bind(socketfd, (struct sockaddr *)(&direccion_propia), size_direccion_propia);
+    bind(socket_server, (struct sockaddr *)(&direccion_propia), size_direccion_propia);
     /* Enlazamos el fichero descriptor del socket con la socket adress de entrada*/
 
-    getsockname(socketfd, (struct sockaddr *)&direccion_propia, &size_direccion_propia);
+    getsockname(socket_server, (struct sockaddr *)&direccion_propia, &size_direccion_propia);
     /* Le pedimos el nombre para que el usuario conozca el puerto */
 
     printf("Esperando en todas las interfaces de red\nPuerto:%d \n", direccion_propia.sin_port);
@@ -87,6 +110,45 @@ void main(int argc, char const *argv[])
 
     games = malloc(5 * sizeof(*games));
     /*Asignamos la memoria para los 5 juegos*/
+
+    /* Ahora empezamos a escuchar para que lleguen los usuarios*/
+
+    listen(socket_server, 10);
+
+    int cliente_esperando = -1;
+
+    /*
+    Creamos una conexión en -1, de esta manera, cuando el accept()
+    reciba una conexión real, actualizará el valor y se podrá trabajar de forma
+    lógica
+    */
+
+    while (1)
+    {
+
+        int cliente_nuevo = accept(socket_server, NULL, NULL);
+        /* Esperamos una conexión en el socket del server, pero no nos interesa quien se conecta */
+
+        if (partidas_activas = 5)
+        {
+            
+        }
+
+        clientes_conectados++;
+        if (cliente_esperando == -1)
+        { /* Si no hay cliente esperando, este empieza a esperar.*/
+            cliente_esperando = cliente_nuevo;
+            send(cliente_esperando, "Esperando contrincante", sizeof("Esperando contrincante"), NULL);
+            continue;
+            /* Se usa un continue para saltarse lo demás y volver a esperar */
+        }
+        games[partidas_activas].jugadores[0] = cliente_esperando;
+        games[partidas_activas].jugadores[1] = cliente_nuevo;
+        partidas_activas++;
+        cliente_esperando = -1;
+        nEmitTask(ejecutar_partida(&games[partidas_activas]));
+        /* Utilizando nSystem llamamos a un subproceso para facilitar esta parte */
+    }
 
     shutdown_server();
     /* Aquí matamos todos los sockets */
